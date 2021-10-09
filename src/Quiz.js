dff7cd37229f31f52ca0b474bca4aae4d4358c3c
JiawenZhuo <>
2021-10-09 23:59:40 +0000

    create QUiz class

---
+++ src/Quiz.js
@@ -0,0 +1,20 @@
+import react, {Component} from "react";
+
+
+let quizData = required("./quiz_data.json");
+
+
+class Quiz extends Component{
+
+  render(){
+    return(
+      <div>text</div>
+    );
+  }
+}
+
+
+
+
+
+export default Quiz;
