fea8b1abde1c854e28b54b7d311ededa7eb46efd
Jiawenzhuo <jiawen.zhuo@uqconnect.edu.au>
2021-10-10 20:31:06 +1000

    debug step 3

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -6,11 +6,11 @@ class QuizQuestion extends Component{
         return(
             <main>
             <section>
-              <p>{this.props}</p>
+              <p>{this.props.quiz_question.instruction_text}</p>
             </section>
             <section className="buttons">
               <ul>
-                  <li>{this.props.quiz_Question.answer_options[0]}</li>
+                  <li>{this.props.quiz_question.answer_options[0]}</li>
               </ul>
             </section>
           </main>
