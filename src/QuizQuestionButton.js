b6d80f8f39b4c4809568682ee33e7c971964b82f
Jiawenzhuo <jiawen.zhuo@uqconnect.edu.au>
2021-10-10 20:52:32 +1000

    step 4 button text

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -4,7 +4,7 @@ import React, {Component} from "react";
 class QuizQuestionButton extends Component{
     render(){
         return (
-            <li><button>{this.props}</button></li>
+            <li><button>{this.props.button_text}</button></li>
         );
     }
 }
