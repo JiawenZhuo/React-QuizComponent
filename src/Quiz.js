46c4afb4f2e17544196ac9388a45f8cca7ed2b6b
JiawenZhuo <>
2021-10-10 00:11:40 +0000

    render with className

--- src/Quiz.js
+++ src/Quiz.js
@@ -7,10 +7,15 @@ let quizData = required("./quiz_data.json");
 class Quiz extends Component{
   constructor(props){
     super(props);
+
+    this.state = {quiz_position: 1};
   }
   render(){
     return(
-      <div>text</div>
+      <div>
+        <div className="QuizQuestion">
+        </div>
+      </div>
     );
   }
 }
