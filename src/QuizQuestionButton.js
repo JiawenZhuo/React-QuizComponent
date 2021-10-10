ebd83b806853cb22b5cb1a63b3acb7fa00dfa9a1
Jiawenzhuo <jiawen.zhuo@uqconnect.edu.au>
2021-10-10 20:42:12 +1000

    replace li to QuizQuestionButton

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+
+class QuizQuestinButton extends Component{
+    render(){
+        return (
+            <li><button></button></li>
+        );
+    }
+}
+
+
+
+
+export default QuizQuestionButton;
